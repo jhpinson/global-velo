6d88f7db3544b2e38a829c54e814f657f2b4171c
Jean-Hugues Pinson <jh.pinson@gmail.com>
2015-03-21 18:29:07 +0100

    teasing

--- settings/production.py
+++ settings/production.py
@@ -1,7 +1,8 @@
 from cache import *
 from common import *
 from pipeline import *
- 
+from os import environ
+
 DEBUG = False
 PIPELINE_AUTO = True
 PIPELINE_VERSION = False
@@ -16,3 +17,17 @@ TEMPLATE_LOADERS = (
         'django.template.loaders.app_directories.Loader',
     )),
 )
+
+
+ALLOWED_HOSTS = 'www.global-velo.pro',
+
+MEDIA_URL = 'http://medias.global-velo.pro/'
+
+STATIC_URL = 'http://statics.global-velo.pro/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'in-v3.mailjet.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = environ.get('MAILJET_USER', '127.0.0.1')
+EMAIL_HOST_PASSWORD = environ.get('MAILJET_PASSWORD', '127.0.0.1')
