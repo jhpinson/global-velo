80cf2ec8f9bbc67104b03a9ca1f1819bef00bbd4
Jean-Hugues Pinson <jh.pinson@gmail.com>
2016-05-28 13:40:15 +0200

    wsgi

--- globalvelo/wsgi.py
+++ globalvelo/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "globalevelo2.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "globalvelo.settings.prod")
 
 application = get_wsgi_application()
